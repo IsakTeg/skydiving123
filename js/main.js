4258d613a0c8ca14e906476cedc570050515ed5d
IsakTeg <isak.malmberg@student.tabyenskilda.se>
2022-05-10 10:46:52 +0200

    BIG UPDATE BIGBIGBIG
    BIG

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Scripten som tillåter texten att komma fram när videon slutar */
 window.onload = function() {
   let video = document.getElementById("myVideo");
   let text = document.getElementById("showText");
@@ -5,4 +6,14 @@ window.onload = function() {
   video.addEventListener("ended", () => {
     text.style.opacity = "1";
   });
+}
+
+/* Scripten som gör att menyn går att öppna och stänga */
+function toggleMenu() {
+  let menu = document.getElementById("links");
+  if (menu.style.width === "0px") {
+    menu.style.width = "7rem"; //Visa meny
+  } else {
+    menu.style.width = "0px"; //Dölj meny
+  }
 }
